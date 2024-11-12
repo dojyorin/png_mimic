a6a0253aae02d6ca12749424a26ab9132b85a3ae
dojyorin <ota_droid@live.jp>
2024-11-13 04:30:30 +0900

    wip

--- src/common.ts
+++ src/common.ts
@@ -17,12 +17,11 @@ const CRC32_TABLE = <const> [
     0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF, 0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94, 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D
 ];
 
-export type ChunkType = "IHDR" | "IDAT" | "IEND" | "gAMA";
-
 export const PNG_BYTE_PER_PIXEL = 3;
 export const PNG_COLOR_DEPTH = 8;
 export const PNG_COLOR_TYPE = 2;
 export const PNG_FILTER = 0;
+export const PNG_CHUNK_NAME_SIZE = 4;
 export const PNG_MAGIC = <const> [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
 
 export function deriveCRC32(...bufs:Uint8Array[]): number {
--- src/decode.ts
+++ src/decode.ts
@@ -1,5 +1,9 @@
-import {byteConcat, deflateDecode, textDecode} from "../deps.ts";
-import {type ChunkType, PNG_BYTE_PER_PIXEL, PNG_COLOR_DEPTH, PNG_COLOR_TYPE, PNG_FILTER, PNG_MAGIC, crc32} from "./common.ts";
+import {PNG_BYTE_PER_PIXEL, PNG_COLOR_DEPTH, PNG_COLOR_TYPE, PNG_FILTER, PNG_CHUNK_NAME_SIZE, PNG_MAGIC, deriveCRC32, compressDecode} from "./common.ts";
+
+interface Chunk {
+    name: string;
+    body: Uint8Array;
+}
 
 /**
 * Extract binary from png image.
@@ -12,48 +16,54 @@ import {type ChunkType, PNG_BYTE_PER_PIXEL, PNG_COLOR_DEPTH, PNG_COLOR_TYPE, PNG
 * ```
 */
 export async function pngDecode(data: Uint8Array): Promise<Uint8Array> {
+    const dec = new TextDecoder();
+
     for(let i = 0; i < PNG_MAGIC.length; i++) {
         if(PNG_MAGIC[i] === data[i]) {
             continue;
         }
 
-        throw new Error();
+        throw new ReferenceError("Invalid magic bytes.");
     }
 
-    const chunk: Partial<Record<ChunkType, Uint8Array>> = {};
-    for(let i = PNG_MAGIC.length; i < data.length;) {
-        const size = new DataView(data.slice(i, i += 4).buffer).getUint32(0);
-        const name = data.slice(i, i += 4);
+    const chunks: Chunk[] = [];
+
+    for(let i = PNG_MAGIC.length; i < data.length; undefined) {
+        const size = new DataView(data.slice(i, i += Uint32Array.BYTES_PER_ELEMENT).buffer).getUint32(0);
+        const name = data.slice(i, i += PNG_CHUNK_NAME_SIZE);
         const body = data.slice(i, i += size);
-        const hash = new DataView(data.slice(i, i += 4).buffer).getInt32(0);
+        const hash = new DataView(data.slice(i, i += Int32Array.BYTES_PER_ELEMENT).buffer).getInt32(0);
 
-        if(crc32(name, body) !== hash) {
-            throw new Error();
+        if(deriveCRC32(name, body) !== hash) {
+            throw new ReferenceError("Checksum mismatch.");
         }
 
-        const key = <ChunkType> textDecode(name);
-
-        if(key in chunk) {
-            continue;
-        }
-
-        chunk[key] = body;
+        chunks.push({
+            name: dec.decode(name),
+            body: body
+        });
     }
 
-    if(!chunk.IHDR || !chunk.gAMA || !chunk.IDAT || !chunk.IEND) {
-        throw new Error();
+    const chunk_IHDR = chunks.find(({name}) => name === "IHDR")?.body;
+    const chunk_IEND = chunks.find(({name}) => name === "IEND")?.body;
+    const chunk_gAMA = chunks.find(({name}) => name === "gAMA")?.body;
+    const chunks_IDAT = chunks.filter(({name}) => name === "IDAT").map(({body}) => body);
+
+    if(!chunk_IHDR || !chunk_IEND || !chunk_gAMA || !chunks_IDAT.length) {
+        throw new ReferenceError("Missing chunks.");
     }
 
-    const image = await deflateDecode(chunk.IDAT, "deflate");
-    const width = new DataView(chunk.IHDR.buffer).getUint32(0);
+    const image = await compressDecode(await new Blob(chunks_IDAT).bytes());
+    const width = new DataView(chunk_IHDR.buffer).getUint32(0);
     const pixel = width * PNG_BYTE_PER_PIXEL;
 
-    if(chunk.IHDR[8] !== PNG_COLOR_DEPTH || chunk.IHDR[9] !== PNG_COLOR_TYPE) {
-        throw new Error();
+    if(chunk_IHDR[8] !== PNG_COLOR_DEPTH || chunk_IHDR[9] !== PNG_COLOR_TYPE) {
+        throw new ReferenceError("Invalid color format.");
     }
 
     const rows: Uint8Array[] = [];
-    for(let i = 0; i < image.byteLength;) {
+
+    for(let i = 0; i < image.byteLength; undefined) {
         if(image[i++] !== PNG_FILTER) {
             i += pixel;
             continue;
@@ -62,5 +72,5 @@ export async function pngDecode(data: Uint8Array): Promise<Uint8Array> {
         rows.push(image.slice(i, i += pixel));
     }
 
-    return byteConcat(...rows).slice(0, -new DataView(chunk.gAMA.buffer).getUint32(0));
+    return (await new Blob(rows).bytes()).slice(0, -new DataView(chunk_gAMA.buffer).getUint32(0));
 }
