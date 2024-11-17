97a710afb5ccf615503a6dfed1f102dbcdd69991
dojyorin <ota_droid@live.jp>
2024-11-17 13:18:56 +0900

    x

--- src/chunk.ts
+++ src/chunk.ts
@@ -1,4 +1,4 @@
-import {MAGIC_MARK, CHUNK_NAME_SIZE, deriveCRC32, byteConcat} from "./common.ts";
+import {MAGIC_MARK, BYTE_PER_PIXEL, CHUNK_NAME_SIZE, FILTER_TYPE, deriveCRC32, byteConcat, compressDecode} from "./common.ts";
 
 interface Chunk {
     name: string;
@@ -13,15 +13,20 @@ interface ChunkIHDR {
     colorType: number;
 }
 
+interface ChunkPLTE {
+    size: number;
+    name: string;
+}
+
 export function* parsePNG(png: Uint8Array): Generator<Chunk> {
     const dec = new TextDecoder();
 
     for(let i = 0; i < MAGIC_MARK.length; i++) {
-        if(png[i] === MAGIC_MARK[i]) {
-            continue;
+        if(png[i] !== MAGIC_MARK[i]) {
+            throw new ReferenceError("Invalid magic bytes.");
         }
 
-        throw new ReferenceError("Invalid magic bytes.");
+        continue;
     }
 
     for(let i = MAGIC_MARK.length; i < png.byteLength; undefined) {
@@ -51,6 +56,31 @@ export function parseIHDR(data: Uint8Array): ChunkIHDR {
         colorDepth: view.getUint8(8),
         colorType: view.getUint8(9)
     };
+}
+
+export function parsePLTE(data: Uint8Array): ChunkPLTE {
+    return {
+        size: new Uint32Array(data.buffer.slice(0, Uint32Array.BYTES_PER_ELEMENT))[0],
+        name: new TextDecoder().decode(data.subarray(Uint32Array.BYTES_PER_ELEMENT)).replaceAll("\0", "")
+    };
+}
+
+export async function parseIDAT(width: number, offset: number, data: Uint8Array): Promise<Uint8Array> {
+    const nbytePerLine = width * BYTE_PER_PIXEL;
+    const image = await compressDecode(data);
+
+    const rows: Uint8Array[] = [];
+
+    for(let i = 0; i < image.byteLength; i++) {
+        if(image[i] !== FILTER_TYPE) {
+            i += nbytePerLine;
+            continue;
+        }
+
+        rows.push(image.slice(i, i += nbytePerLine));
+    }
+
+    return byteConcat(...rows).slice(0, -offset);
 }
 
 function createChunk(name: string, data: Uint8Array) {
