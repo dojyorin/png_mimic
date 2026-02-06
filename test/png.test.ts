498a8a74d4253f30c40873909de88e31cb08170e
dojyorin <ota_droid@live.jp>
2026-02-07 06:15:55 +0900

    merge.

--- test/png.test.ts
+++ test/png.test.ts
@@ -1,10 +1,19 @@
 import {assertEquals} from "@std/assert";
-import {decode} from "../src/png.ts";
+import {encode, decode} from "../src/png.ts";
 
 const [bin, png] = await Promise.all([
     Deno.readFile(new URL(import.meta.resolve("./assets/sample.bin"))),
     Deno.readFile(new URL(import.meta.resolve("./assets/sample.png")))
 ]);
+
+Deno.test({
+    name: "Encode",
+    async fn() {
+        const output = await encode(bin);
+
+        assertEquals(output, png);
+    }
+});
 
 Deno.test({
     name: "Decode",
